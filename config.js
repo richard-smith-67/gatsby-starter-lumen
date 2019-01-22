245b7fc076bd923070e895756ed202437acea518
Richard Smith <richard.smith.67@gmail.com>
2019-01-22 21:06:37 +0000

    initial commit

--- config.js
+++ config.js
@@ -1,38 +1,40 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
+  url: "https://lumen.netlify.com",
+  title: "Blog by Richard Smith",
+  subtitle:
+    "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: "UA-73379983-2",
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/"
     },
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: "About me",
+      path: "/pages/about"
     },
     {
-      label: 'Contact me',
-      path: '/pages/contacts'
+      label: "Contact me",
+      path: "/pages/contacts"
     }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: "Richard Smith",
+    photo: "/elvis.jpg",
+    bio:
+      "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
     contacts: {
-      email: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '#',
-      vkontakte: '#'
+      email: "#",
+      telegram: "#",
+      twitter: "#",
+      github: "#",
+      rss: "#",
+      vkontakte: "#"
     }
   }
 };
---
+++ content/posts/2019-22-01---Netlify-Is-Amazing.md
@@ -0,0 +1,14 @@
+---
+title: Netlify is amazing
+date: "2019-01-22T20:40:32.169Z"
+template: "post"
+draft: false
+slug: "/posts/netlify-is-amazing/"
+category: "DevOps"
+tags:
+  - "Design"
+  - "Web Development"
+description: "How DevOps came to rule..."
+---
+
+God this is so much better than the shit I work with
